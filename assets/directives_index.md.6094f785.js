1bfc26762737eb52f8ce64e1bd18a450123ef705
rash <rash@rash.codes>
2022-10-01 20:47:19 +0200

    Updates

---
+++ assets/directives_index.md.6094f785.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as r}from"./app.759ecea3.js";const m=JSON.parse('{"title":"Directives","description":"","frontmatter":{"title":"Directives"},"headers":[],"relativePath":"directives/index.md"}'),i={name:"directives/index.md"};function s(a,c,n,o,d,p){return t(),r("div")}const f=e(i,[["render",s]]);export{m as __pageData,f as default};
